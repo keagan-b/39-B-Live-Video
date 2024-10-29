a96e161c9fe8a0af67c9826661a6060d394d09f0
Keagan <46222836+keagan-b@users.noreply.github.com>
2024-10-29 17:04:24 -0400

    git link correction

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
-git pull git@github.com:keagan-b/39-B-Live-Video.git
+git pull https://github.com/keagan-b/39-B-Live-Video.git
 ./.venv/bin/pip install -r requirements.txt
 ./.venv/bin/py ./src/main.py
